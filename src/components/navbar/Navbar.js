c66782ba03def3d0632a757d21633ea69de3a531
Wiktor Kościelny <wiktorkoscielny@gmail.com>
2022-03-16 11:56:58 +0100

    Finished navbar component, added som style in navbar.css

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,53 +1,54 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import './Navbar.css'
 
 function Navbar() {
 
-const [click, setClick] = useState(false);
+    const [click, setClick] = useState(false);
 
-const handleClick = () => {
-    setClick(!click)
+    const handleClick = () => {
+        setClick(!click)
+    };
+
+    const closeMobileMenu = () => setClick(false);
+
+    return (
+
+        <>
+            <nav className='navbar'>
+                <Link to='/' 
+                    className='navbar-logo'>
+                    EPIC <i className='fab fa-firstdraft' />
+                </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+
+                    <li className='nav-item'>
+                        <Link to='/search-page' className='nav-links' onClick={closeMobileMenu}>
+                            Search
+                        </Link>
+                    </li>
+
+                    <li className='nav-item'>
+                        <Link to='/employee-page' className='nav-links' onClick={closeMobileMenu}>
+                            Add Employee
+                        </Link>
+                    </li>
+
+                </ul>
+
+            </nav>
+        </>
+    )
+
 }
-
-const closeMobileMenu = () => setClick(false)
-
-return (
-
-<>
-    <nav className='navbar'>
-        <Link to='/' 
-            className='navbar-logo'>
-            EPIC <i className='fab fa-firstdraft' />
-        </Link>
-        <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-        </div>
-
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-
-            <li className='nav-item'>
-                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                    Home
-                </Link>
-            </li>
-
-            <li className='nav-item'>
-                <Link to='/search-page' className='nav-links' onClick={closeMobileMenu}>
-                    Search
-                </Link>
-            </li>
-
-            <li className='nav-item'>
-                <Link to='/employee-page' className='nav-links' onClick={closeMobileMenu}>
-                    Add Employee
-                </Link>
-            </li>
-
-        </ul>
-
-    </nav>
-</>
-
-)
-
-}
+export default Navbar
